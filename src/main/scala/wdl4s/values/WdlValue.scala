d187a6e51a81febd7e2cacd266f684d7c8e80968
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-05-18 15:20:56 -0700

    Refactor SymbolHash aggregation

--- src/main/scala/wdl4s/values/WdlValue.scala
+++ src/main/scala/wdl4s/values/WdlValue.scala
@@ -47,21 +47,13 @@ trait WdlValue {
     if (filterFn.isDefinedAt(this)) Seq(filterFn(this)) else Nil
   }
 
-  private def symbolHash(hash: String) = SymbolHash((this.getClass.getCanonicalName + hash).md5Sum)
-
-  private def symbolHash[K](hashedMap: Map[K, SymbolHash])(implicit ord: Ordering[K]): SymbolHash = {
-    // productIterator returns an Iterator over the elements of a Tuple2 Map entry.
-    val concatenatedMap = TreeMap(hashedMap.toArray: _*) flatMap { _.productIterator } mkString ""
-    symbolHash(concatenatedMap)
-  }
-
   def computeHash(implicit hasher: FileHasher): SymbolHash = {
     this match {
-      case w: WdlObject => symbolHash(w.value mapValues { _.computeHash(hasher) })
-      case w: WdlMap => symbolHash(w.value map { case (k, v) => k.computeHash(hasher) -> v.computeHash(hasher) })
-      case w: WdlArray => symbolHash(w.value map { _.computeHash(hasher) } mkString "")
+      case w: WdlObject => SymbolHash(w.value mapValues { _.computeHash(hasher) })
+      case w: WdlMap => SymbolHash(w.value map { case (k, v) => k.computeHash(hasher) -> v.computeHash(hasher) })
+      case w: WdlArray => SymbolHash(w.value map { _.computeHash(hasher) })
       case w: WdlFile => hasher(w)
-      case w => symbolHash(w.valueString)
+      case w => SymbolHash(w.getClass, w.valueString)
     }
   }
 }
--- src/main/scala/wdl4s/values/package.scala
+++ src/main/scala/wdl4s/values/package.scala
@@ -2,8 +2,19 @@ package wdl4s
 
 import org.apache.commons.codec.digest.DigestUtils
 
+import scala.collection.immutable.TreeMap
 
 package object values {
+
+  object SymbolHash {
+    def apply(clazz: Class[_ <: WdlValue], hash: String): SymbolHash = SymbolHash((clazz.getCanonicalName + hash).md5Sum)
+    def apply(l: Seq[SymbolHash]): SymbolHash = apply(classOf[WdlArray], l.foldLeft("")((x, y) => x + y.value))
+    def apply[K](hashedMap: Map[K, SymbolHash])(implicit ord: Ordering[K]): SymbolHash = {
+      // productIterator returns an Iterator over the elements of a Tuple2 Map entry.
+      val concatenatedMap = TreeMap(hashedMap.toArray: _*) flatMap { _.productIterator } mkString ""
+      apply(classOf[WdlMap], concatenatedMap)
+    }
+  }
 
   case class SymbolHash(value: String) extends AnyVal with Ordered[SymbolHash] {
     def compare(that: SymbolHash) = this.value compare that.value
