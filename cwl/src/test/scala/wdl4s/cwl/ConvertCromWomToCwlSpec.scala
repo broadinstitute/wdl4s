7cb2968e09fd2a1c47ef68d5c4146a231c5e316f
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-07-28 20:03:36 +0200

    Slight addition to TaskDefinition instantiation stub.

--- cwl/src/test/scala/wdl4s/cwl/ConvertCromWomToCwlSpec.scala
+++ cwl/src/test/scala/wdl4s/cwl/ConvertCromWomToCwlSpec.scala
@@ -1,8 +1,8 @@
 package wdl4s.cwl
 
 import org.scalatest.{FlatSpec, Matchers}
-import wdl4s.wdl.RuntimeAttributes
-import wdl4s.wdl.command.CommandPart
+import wdl4s.wdl.{RuntimeAttributes, WdlExpression}
+import wdl4s.wdl.command.{CommandPart, ParameterCommandPart, StringCommandPart}
 import wdl4s.wom.callable.{Callable, TaskDefinition}
 import wdl4s.wom.expression.Expression
 
@@ -10,8 +10,14 @@ class ConvertCromWomToCwlSpec extends FlatSpec with Matchers {
 
   it should "convert CromWOM TaskDefinition to CWL CommandLineTool" in {
     val taskDef = TaskDefinition(
-      name = ??? : String,
-      commandTemplate = ??? : Seq[CommandPart],
+      name = "message",
+      commandTemplate = Seq(
+        StringCommandPart("echo "),
+        ParameterCommandPart(
+          attributes = ??? : Map[String, String],
+          expression = ??? : WdlExpression
+        )
+      ) : Seq[CommandPart],
       runtimeAttributes = ??? : RuntimeAttributes,
       meta = ??? : Map[String, String],
       parameterMeta = ??? : Map[String, String],
