47c9e49061ddd5008b1c0f965a597a489b303d67
Dan Billings <danbills2live.com>
2017-08-21 08:30:30 -0400

    test skeleton

---
+++ cwl/src/test/scala/wdl4s/cwl/CwlDecoderSpec.scala
@@ -0,0 +1,34 @@
+package wdl4s.cwl
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class CwlDecoderSpec extends FlatSpec with Matchers {
+  "" should "" in {
+
+    val cwl = Coproduct[Cwl](Workflow.create(
+      name = generate[String]
+      ))
+    /*
+    property("make mine a half") {
+      forAll { (n: Int) =>
+        whenever(n > 1) { n / 2 should be > 0 }
+      }
+    }
+    */
+
+   property("create a workflow that will replace its steps with the actual object it represents") {
+     forAll(asciiString, asciiString) {
+       (workflowName, commandLineToolName) =>
+         //create a file with  this string, assign to a Cwl file run step, and read it back as a result"
+         val clt = CommandLineTool.create(commandLineToolName)
+         val wfw = Workfow.create(steps = List(WorfklowStep.create(run = clt)))
+
+         encodeCwl(wfw) |> decodeAllCwl
+
+         //Separates out the cwl into nice files that
+         encodeAsLinkedFiles(wfw):
+         encodeCwl
+  }
+
+
+}
