f076541820de7e5da77a6c8f189ccd1fdc681942
Thibault Jeandet <tjeandet@broadinstitute.org>
2017-09-12 16:03:44 -0400

    helper lazy val

--- wom/src/main/scala/wdl4s/wom/callable/WorkflowDefinition.scala
+++ wom/src/main/scala/wdl4s/wom/callable/WorkflowDefinition.scala
@@ -1,7 +1,7 @@
 package wdl4s.wom.callable
 
 import lenthall.validation.ErrorOr.ErrorOr
-import wdl4s.wom.graph.Graph
+import wdl4s.wom.graph.{Graph, TaskCallNode}
 import cats.syntax.validated._
 import wdl4s.wom.expression.WomExpression
 import wdl4s.wom.graph.GraphNode._
@@ -17,4 +17,8 @@ final case class WorkflowDefinition(name: String,
 
   // FIXME: how to get a meaningful order from the node set ?
   override lazy val inputs: List[_ <: Callable.InputDefinition] = innerGraph.nodes.inputDefinitions.toList
+
+  lazy val taskCallNodes: Set[TaskCallNode] = innerGraph.nodes collect {
+    case taskNode: TaskCallNode => taskNode
+  }
 }
