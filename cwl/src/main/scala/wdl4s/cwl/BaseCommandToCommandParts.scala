6da2079d6de6d526d208c5580872d6f46fb73d58
Thibault Jeandet <tjeandet@broadinstitute.org>
2017-09-13 15:22:54 -0400

    PR comments

--- cwl/src/main/scala/wdl4s/cwl/BaseCommandToCommandParts.scala
+++ cwl/src/main/scala/wdl4s/cwl/BaseCommandToCommandParts.scala
@@ -5,7 +5,7 @@ import wdl4s.wdl.command.StringCommandPart
 import wdl4s.wom.CommandPart
 
 object BaseCommandToCommandParts extends Poly1 {
-  implicit def one = at[String] { baseCommand => Seq(baseCommand).map(StringCommandPart.apply): Seq[CommandPart] }
+  implicit def one = at[String] { Seq(_).map(StringCommandPart.apply): Seq[CommandPart] }
 
   implicit def many = at[Array[String]] { _.toSeq.map(StringCommandPart.apply): Seq[CommandPart] }
 }
